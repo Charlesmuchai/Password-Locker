7221e9820b90a1d3fa82e4185e41b7a084cc046c
Charlesmuchai <charlieblacks328@icloud.com>
2018-01-19 17:05:12 +0300

    Add Readme file

--- users.py
+++ users.py
@@ -3,10 +3,14 @@ class User:
     users_list = []
 
     def __init__(self,user_name,password):
-        """
-        Args:
-            self.user_name = user_name
-            self.password = password
-        """
+
         self.user_name = user_name
         self.password = password
+
+    def create_users(self):
+
+        User.users_list.append(self)
+
+    def save_users(self):
+
+        User.users_list.append(self)
--- users_test.py
+++ users_test.py
@@ -2,3 +2,36 @@ import unittest
 from users import User
 
 class TestUser(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_users = User("Ian","12345")
+
+    def tearDown(self):
+        User.users_list = []
+
+    def test_init(self):
+
+        self.assertEqual(self.new_users.user_name,"Ian")
+        self.assertEqual(self.new_users.password,"12345")
+
+
+    def test_create_users(self):
+
+        self.new_users.create_users()
+        self.assertEqual(len(User.users_list),1)
+
+    def test_save_users(self):
+
+        self.new_users.save_users()
+        self.assertEqual(len(User.users_list),1)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   unittest.main()
